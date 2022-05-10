99fba4741297b3d5dab1af153adfb47dc1f3a634
“charlesdccti” <charlesdccti@gmail.com>
2022-05-10 17:25:51 -0300

    Union of two principles, LSP with ISP

---
+++ src/com/udemy/solid/MainISP.java
@@ -0,0 +1,16 @@
+package com.udemy.solid;
+
+import com.udemy.solid.srp.interfaces.Drivable;
+import com.udemy.solid.srp.models.Drive;
+
+public class MainISP {
+
+    public static void main(String[] args) {
+        
+        teste(new Drive());
+    }
+
+    public static void teste(Drivable drivable) {
+        
+    }
+}
